2215fa46e9b65976f3e43f28110134dbcd4f7565
Matt Wagner <mattgwagner@gmail.com>
2020-05-07 21:43:10 -0400

    Add trello API key ref

---
+++ Index.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Manatee.Trello;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace One_on_Ones
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public async Task OnGet()
+        {
+            // Get an app key via https://trello.com/app-key
+            // Then get a 'server token' since we are too lazy to bother with OAuth flow
+
+            var trello = new TrelloFactory().Me(new TrelloAuthorization
+            {
+                AppKey = "Wags-Test",
+                UserToken = ""
+            });
+
+        }
+    }
+}
