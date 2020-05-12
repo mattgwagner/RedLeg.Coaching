460964db8f8e7760d689bd2a66d2685b6db64705
Matt Wagner <mattgwagner@gmail.com>
2020-05-11 21:38:26 -0400

    Wire up Auth0
    Fixes #1

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RedLeg.Coaching.Pages
+{
+    [AllowAnonymous]
+    public class LoginModel : PageModel
+    {
+        public IActionResult OnGet(string returnUrl = "/")
+        {
+            return new ChallengeResult("Auth0", new Microsoft.AspNetCore.Authentication.AuthenticationProperties
+            {
+                RedirectUri = returnUrl
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,32 +1,77 @@
 using Manatee.Trello;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace RedLeg.Coaching
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
-            // Get an app key via https://trello.com/app-key
-            // Then get a 'server token' since we are too lazy to bother with OAuth flow
-
-            TrelloAuthorization.Default.AppKey = configuration.GetValue<string>("Trello:AppKey");
-            TrelloAuthorization.Default.UserToken = configuration.GetValue<string>("Trello:ApiKey");
+            Configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Get an app key via https://trello.com/app-key
+            // Then get a 'server token' since we are too lazy to bother with OAuth flow
+
+            TrelloAuthorization.Default.AppKey = Configuration.GetValue<string>("Trello:AppKey");
+            TrelloAuthorization.Default.UserToken = Configuration.GetValue<string>("Trello:ApiKey");
+
             services.AddOptions<TrelloConfiguration>("Trello");
 
-            services.AddRazorPages();
+            services
+                .AddRazorPages()
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.AuthorizePage("/Index");
+                });
 
             services.AddSingleton<ITrelloFactory, TrelloFactory>();
+
+            // Add authentication services
+
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(o => o.LoginPath = new PathString("/Login"))
+                .AddOpenIdConnect("Auth0", options =>
+                {
+                    options.Authority = $"https://{Configuration["Auth0:Domain"]}";
+
+                    options.ClientId = Configuration["Auth0:ClientId"];
+                    options.ClientSecret = Configuration["Auth0:ClientSecret"];
+
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+
+                    // Configure the scopes
+                    options.Scope.Clear();
+                    options.Scope.Add("profile");
+                    options.Scope.Add("openid");
+                    options.Scope.Add("email");
+
+                    // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0
+                    // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+                    options.CallbackPath = new PathString("/signin-auth0");
+
+                    // Configure the Claims Issuer to be Auth0
+                    options.ClaimsIssuer = "Auth0";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +83,9 @@ namespace RedLeg.Coaching
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
