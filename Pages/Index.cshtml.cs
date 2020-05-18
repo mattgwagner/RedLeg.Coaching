8acfe582080ec1943cdd5cc09861d3ebb9b9129b
Matt Wagner <mattgwagner@gmail.com>
2020-05-17 20:44:20 -0400

    For non-admins, checkc the email on the card

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 using Manatee.Trello;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace RedLeg.Coaching
@@ -13,6 +15,10 @@ namespace RedLeg.Coaching
 
         private readonly IOptions<TrelloConfiguration> configuration;
 
+        public string Email => User.FindFirstValue(ClaimTypes.Email);
+
+        public Boolean IsAdmin { get; private set; }
+
         public IDictionary<IBoard, IDictionary<IList, IEnumerable<ICard>>> Data { get; private set; } = new Dictionary<IBoard, IDictionary<IList, IEnumerable<ICard>>>();
 
         public IndexModel(ITrelloFactory factory, IOptions<TrelloConfiguration> configuration)
@@ -21,23 +27,47 @@ namespace RedLeg.Coaching
             this.configuration = configuration;
         }
 
+        protected async Task<Boolean> Should_Display(ICard card)
+        {
+            if (card.IsArchived == true) return false;
+
+            if (!card.Labels.Any(label => label.Name == "One-on-One")) return false;
+
+            await card.Refresh();
+
+            if (IsAdmin) return true;
+
+            var custom_field = card.CustomFields.FirstOrDefault() as CustomField<string>;
+
+            if (custom_field != null)
+            {
+                return string.Equals(Email, custom_field.Value, StringComparison.InvariantCultureIgnoreCase);
+            }
+
+            return false;
+        }
+
         public async Task OnGet()
         {
             var me = await factory.Me();
 
             await me.Refresh(force: true);
 
-            //var my_board = new Board(configuration.Value.BoardId);
+            // Is the user I'm logged in as match the admin email of the api keys?
 
-            //Data.Add(my_board, my_board.Cards.Select(card => card));
+            IsAdmin = string.Equals(me.Email, Email, StringComparison.InvariantCultureIgnoreCase);
 
             foreach (var board in me.Boards)
             {
+                // Go through each board, but we're only display cards on the configured board of interest
+
                 if (board.Id == configuration.Value.BoardId)
                 {
                     await board.Refresh();
 
                     var list_data = new Dictionary<IList, IEnumerable<ICard>>();
+
+                    // Go through all of the lists and collect up non-archived cards with the `One-on-One` tag
 
                     foreach (var list in board.Lists)
                     {
@@ -47,14 +77,9 @@ namespace RedLeg.Coaching
 
                         foreach (var card in list.Cards)
                         {
-                            if (card.IsArchived == false)
+                            if (await Should_Display(card))
                             {
-                                if (card.Labels.Any(label => label.Name == "One-on-One"))
-                                {
-                                    await card.Refresh();
-
-                                    cards.Add(card);
-                                }
+                                cards.Add(card);
                             }
                         }
 
